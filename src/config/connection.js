160529e99ce1fcd34e159bf9db4256447c30a730
MAYURESHBAILURKAR <mayureshbailurkar@gmail.com>
2023-01-08 14:53:07 +0530

    changes Added

--- src/config/connection.js
+++ src/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = async () => {
-  // mongoose.set("strictQuery", false);
+  mongoose.set("strictQuery", false);
   return await connect(process.env.mongoDB_URL);
 };
 
